516844e5bb2ab123043c57a72fcf71ec2db17c51
ksukriti08 <40966384+ksukriti08@users.noreply.github.com>
2024-12-13 08:40:16 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import ROSLIB from 'roslib';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Path from "./Path";  
+import Start from "./Start";  
 
 // using routes to navigate between pages
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Start />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/Path/:room" element={<Path />} /> 
       </Routes>
     </Router>
