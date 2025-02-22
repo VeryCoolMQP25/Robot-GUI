c2cc883add3e87f040c4bc7289c86ad1a5b81369
aashigoel03 <aashigoel03@gmail.com>
2025-02-22 01:06:23 -0500

    added home button

--- src/Speaker.js
+++ src/Speaker.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
 import './Start.css';
 
 function Home() {
   const [statusMessage, setStatusMessage] = useState("");
   const [output, setOutput] = useState("");
+  const navigate = useNavigate();
 
   const speaker = () => {
     setStatusMessage("Please wait.....");
@@ -23,13 +25,22 @@ function Home() {
   };
 
   return (
-    <div className="app">
-      <p>Please click on the microphone when you are ready & speak clearly!</p>
-      <button onClick={speaker}>
-        <img src="https://www.iconpacks.net/icons/1/free-microphone-icon-342-thumb.png" alt="Microphone" />
+    <div className="explore-unity-page">
+      <button 
+        className="home-choice-button" 
+        onClick={() => navigate("/")}
+      >
+        Home
       </button>
-      <p>{statusMessage}</p>
-      <pre>{output}</pre>  {/* Display output from the Python script */}
+
+      <div className="app">
+        <p>Please click on the microphone when you are ready & speak clearly!</p>
+        <button onClick={speaker}>
+          <img src="https://www.iconpacks.net/icons/1/free-microphone-icon-342-thumb.png" alt="Microphone" />
+        </button>
+        <p>{statusMessage}</p>
+        <pre>{output}</pre>  {/* Display output from the Python script */}
+      </div>
     </div>
   );
 }
