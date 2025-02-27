08a30d4f82c51ed179605b9b5bedc74bddd67be7
ksukriti08 <ksukriti08@gmail.com>
2025-02-27 13:29:30 -0500

    cleaned up

--- src/Start.js
+++ src/Start.js
@@ -8,21 +8,21 @@ function Start() {
   return (
     <div className="start-page">
       <h1>Welcome to Unity Hall!</h1>
-      <p>Choose your journey:</p>
+      {/* <p>Choose your journey:</p> */}
 
       <div className="button-container">
         <div className="option">
           <button className="choice-button" onClick={() => navigate("/destination-input")}>
             Take Me To...
           </button>
-          <p className="caption">(If you know where you're going)</p>
+          {/* <p className="caption">(If you know where you're going)</p> */}
         </div>
 
         <div className="option">
           <button className="choice-button" onClick={() => navigate("/explore-unity")}>
             Explore Unity
           </button>
-          <p className="caption">(If you're feeling adventurous)</p>
+          {/* <p className="caption">(If you're feeling adventurous)</p> */}
         </div>
 
         <div className="option">
