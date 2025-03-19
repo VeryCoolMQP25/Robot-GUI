dd77def14cc659568f87db8a9f925639605a847c
aashigoel03 <aashigoel03@gmail.com>
2025-03-18 21:10:50 -0400

    added useState to Start.js

--- src/Start.js
+++ src/Start.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Start.css";
 import toriGreeting from "./tori-greeting.mp3"; 
