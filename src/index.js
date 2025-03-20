57596f3b667cdbd297eef93a2cd7f6edc188fee8
aashigoel03 <aashigoel03@gmail.com>
2025-03-20 12:17:03 -0400

    adding aashi's changes to suki's main branch

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import { RosProvider } from './RosContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RosProvider>
-      <App />
-    </RosProvider>
-  </React.StrictMode>
+  <RosProvider>
+    <App />
+  </RosProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
